6e9778ea1001aa614f5dd2018e829aeb047e3821
Смирнов Дмитрий Дмитриевич <d.smirnov@voskhod.ru>
2021-07-27 17:45:05 +0300

    +

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"ex-011-go-web-jpa-postgres/controller"
 	"fmt"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -49,13 +50,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("Connected to MongoDB!")
-	collection := client.Database("ex-011-database").Collection("trainers")
 
+	collection := client.Database("ex-011-database").Collection("trainers")
 	ash := Trainer{"Ash", 10, "Pallet Town"}
 	misty := Trainer{"Misty", 10, "Cerulean City"}
-	brock := Trainer{"Brock", 15, "Pewter City"}
+	brock := Trainer{"Brock", 10, "Pewter City"}
+
 	insertResult, err := collection.InsertOne(context.TODO(), ash)
 	if err != nil {
 		log.Fatal(err)
@@ -68,6 +69,67 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
+
+	updateFilter := bson.D{{"name", "Ash"}}
+	update := bson.D{
+		{"$inc", bson.D{
+			{"age", 1},
+		}},
+	}
+	updateResult, err := collection.UpdateOne(context.TODO(), updateFilter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+
+	// create a value into which the result can be decoded
+	var result Trainer
+	err = collection.FindOne(context.TODO(), updateFilter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Found a single document: %+v\n", result)
+
+
+	// Pass these options to the Find method
+	options := options.Find()
+	options.SetLimit(20)
+	filter := bson.M{}
+	// Here's an array in which you can store the decoded documents
+	var results []*Trainer
+	// Passing nil as the filter matches all documents in the collection
+	cur, err := collection.Find(context.TODO(), filter, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Finding multiple documents returns a cursor
+	// Iterating through the cursor allows us to decode documents one at a time
+	for cur.Next(context.TODO()) {
+		// create a value into which the single document can be decoded
+		var elem Trainer
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, &elem)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+	fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
+
+	deleteFilter := bson.D{{"age", 10}}
+	deleteResult, err := collection.DeleteMany(context.TODO(), deleteFilter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
+
+
+
+	/* MAIN CODE */
 
 	router := mux.NewRouter()
 	router.Use(app.JwtAuthentication) // добавляем middleware проверки JWT-токена
